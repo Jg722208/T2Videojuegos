9bd3297287a55fecf40fc95e42593cd0613ee14e
Jg722208 <abc@gmail.com>
2020-10-16 13:49:57 -0500

    Proy T2

---
+++ Assets/Script/ZombiMan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiMan : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float velocidad = 2f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(velocidad *-1f, rb.velocity.y);
+    }
+}
---
+++ Assets/Script/ZombieHacer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHacer : MonoBehaviour
+{
+    public GameObject Zombie;
+    private Transform _transform;
+    private float tiempo = 0f;
+
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+
+        tiempo += Time.deltaTime;
+        Debug.Log(tiempo);
+        if (tiempo >= 4)
+        {
+            Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+            Instantiate(Zombie, _transform.position, Quaternion.identity);
+            tiempo = 0;
+        }
+    }
+}
